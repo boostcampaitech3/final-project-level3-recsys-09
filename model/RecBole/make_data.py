9ed979bf2b18160b012da5d7722066b84d6867b7
lg <lg960214@naver.com>
2022-05-31 12:31:24 +0000

    convert mongodb data to .inter file

--- model/RecBole/make_data.py
+++ model/RecBole/make_data.py
@@ -1,19 +1,44 @@
+import pymongo
 import pandas as pd
 import os
 
-FILE = "/opt/ml/input/data/train/train_ratings.csv"
-TARGET_DIR = os.path.join(os.getcwd(), "data/boostcamp")
+# db에서 데이터 불러오기
+
+client = pymongo.MongoClient("mongodb://118.67.143.144:30001/")
+
+db = client["amazon"]
+collection = db["train"]
+
+cursor = collection.find(
+    {},
+    projection={
+        "_id": False,
+        "reviewerID": True,
+        "asin": True,
+        "overall": True,
+        "unixReviewTime": True,
+    },
+)
+
+df = pd.DataFrame.from_dict(cursor)
+df.columns = ["userID", "itemID", "rating", "timestamp"]
+
+
+# .inter file 생성
+
+TARGET_DIR = os.path.join(os.getcwd(), "/workspace/data/boostcamp")
 TARGET_NAME = "boostcamp.inter"
 
 os.makedirs(TARGET_DIR, exist_ok=True)
 
-df = pd.read_csv(FILE)
 df = df.rename(
     columns={
         "user": "user_id:token",
         "item": "item_id:token",
+        "rating": "rating:float",
         "time": "timestamp:float",
     }
 )
 df.to_csv(os.path.join(TARGET_DIR, TARGET_NAME), index=False, sep="\t")
+
 print("Done!")
