125a8e2d41181080f2899a24948680aeb804923c
SYB <dpqls9799@naver.com>
2022-06-03 00:02:42 +0900

    6/2

--- app/app.py
+++ app/app.py
@@ -35,7 +35,6 @@ app.add_middleware(
 )
 
 df = pd.read_csv("book.csv")
-#a = df.set_index('asin').T.to_dict('list')
 b = list(df['asin'])
 
 @app.get("/rand/{idx}")
@@ -43,23 +42,7 @@ def read_item1(idx):
     time.sleep(0.1)
     return random.sample(b, int(idx))
 
-@app.get("/search/{asin}")
-def read_item2(asin):
-    time.sleep(0.54)
-    '''
-    url = "https://dapi.kakao.com//v3/search/book"
-    MY_REST_API = 'a9f59d8981601a41b5fb31311b82b8a4'
-    input_text = asin
-    queryString = {'query' : input_text}
-    header = {'Authorization': 'KakaoAK ' + MY_REST_API }
-    response = requests.get(url, headers=header, params=queryString)
-    tokens = response.json()
-    try:    
-        return tokens["documents"][0]["thumbnail"]
-    except:
-        return "https://upload.wikimedia.org/wikipedia/commons/thumb/7/75/Flag_of_None_%28square%29.svg/768px-Flag_of_None_%28square%29.svg.png"
-    '''
-    
+def api_load_image(asin):
     input_text = asin
     client_id = "EZKJjFYCeCFKfj3qk2OM"
     client_secret = "dLMxOkCV6M"
@@ -76,83 +59,11 @@ def read_item2(asin):
         tokens = json.loads(Response.decode('utf-8'))
         return tokens['items'][0]['image']
     except:
-        #print("Error Code:", success)
         return "https://bookthumb-phinf.pstatic.net/cover/061/154/06115486.jpg?type=m1&udate=20210317"
-    
-@app.get("/imglist/{lists}")
-def get_imglist(lists):
-    time.sleep(0.1)
-    strings = lists.replace("%2c", "")
-    strings = strings.split(",")
-    strings = [s for s in strings if s != ""]
-    img_url = []
-    
-    for s in strings:
-        img_url.append(read_item2(s))
-    
-    return img_url
 
-    
-@app.get("/db/{asin}")
-def get_item(asin):
-    
-
-    client = MongoClient(mongodb_client)
-    db = client["amazon"]
-    collection = db["books"]
-    query = {'asin': asin}
-    cursor = collection.find(query, projection={'_id': False})
-    result = loads(dumps(cursor))
-    return result
-
-@app.get("/ddb/{asin}")
-def get_item(asin):
-    client = pymongo.MongoClient(mongodb_client)
-    db = client["amazon"]
-    collection = db["books"]
-    naver_client_id = "EZKJjFYCeCFKfj3qk2OM"
-    naver_client_secret = "dLMxOkCV6M"
-    kakao_API_url = "https://dapi.kakao.com//v3/search/book"
-    Kakao_MY_REST_API = 'a9f59d8981601a41b5fb31311b82b8a4'
-    # ↑--- 고정 ---↑
-    input_text = asin
-    query = {'asin': input_text}
-    cursor = collection.find(query)
-    '''
-    if cursor['imageURL'] == '':
-        # Naver API search
-        naver_input_text_n = urllib.parse.quote(input_text)
-        naver_url = "https://openapi.naver.com/v1/search/book?query=" + naver_input_text_n +"&display=3&sort=count"
-        Send_request = urllib.request.Request(naver_url)
-        Send_request.add_header("X-Naver-Client-Id", naver_client_id)
-        Send_request.add_header("X-Naver-Client-Secret", naver_client_secret)
-        response = urllib.request.urlopen(Send_request)
-        success = response.getcode()
-        if (success == 200):
-            Response = response.read()
-            tokens = json.loads(Response.decode('utf-8'))
-            return tokens['items'][0]['image']
-        else:
-            # Kakao API search
-            queryString = {'query' : input_text}
-            header = {'Authorization': 'KakaoAK ' + Kakao_MY_REST_API }
-            response = requests.get(kakao_API_url, headers=header, params=queryString)
-            tokens = response.json()
-            
-            try:    
-                return tokens["documents"][0]["thumbnail"]
-            except:
-                return "https://upload.wikimedia.org/wikipedia/commons/thumb/7/75/Flag_of_None_%28square%29.svg/768px-Flag_of_None_%28square%29.svg.png"
-    else:
-        return cursor['imageURL']
-    '''
-    
-    for record in cursor:
-        return record
-
-
-@app.get("/dddb/{asin}")
-def get_1111(asin):
+@app.get("/search/{asin}")
+def load_image(asin):
+    time.sleep(0.54)
     client = pymongo.MongoClient(mongodb_client)
     # db(=database): "recsys09"의 "amazon" 데이터베이스
     db = client["amazon"]
@@ -164,7 +75,7 @@ def get_1111(asin):
     try:
         return cursor['image_url'][0]
     except:
-        return read_item2(asin)
+        return api_load_image(asin)
     
 @app.get("/inference/{lists}")
 def getpredict(lists):
@@ -189,9 +100,32 @@ def getpredict(lists):
                                                   k=10,
                                                   remove_seen=True
                                                   )
-    # Convert sparse matrix back to df
+    
     top_k_df = am_inference.map_back_sparse(top_k, kind='prediction')
 
     return list(top_k_df['itemID'].values)
     
-    #return list(top_k)
+li = []
+class Inter(BaseModel):
+    id: str
+    rate: int
+    review: str
+    
+@app.post("/intersave")
+def test(inter:Inter):
+    li.append(inter)
+    
+@app.get("/test2")
+def t2():
+    return(li)
+    
+@app.get("/items/{asin}")
+def get_item(asin):
+    time.sleep(0.54)
+    client = MongoClient(mongodb_client)
+    db = client["amazon"]
+    collection = db["books"]
+    query = {'asin': asin}
+    cursor = collection.find(query, projection={'_id': False})
+    result = loads(dumps(cursor))
+    return result
